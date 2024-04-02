6770a1462ceb84675541d19c2996d844e8bd0cc0
HugoQ2001 <hugo.quispe12359@gmail.com>
2024-04-01 22:15:04 -0500

    added folder components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import {Button} from '@nextui-org/button';
+import Navbar from './components/navbar';
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen">
+      
+      <Navbar />
       <Button>Click me</Button>
     </main>
   );
 }
+  
