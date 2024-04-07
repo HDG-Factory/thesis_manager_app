2851b9aab3d323342056d7c3d3939e8003ac8b48
HugoQ2001 <hugo.quispe12359@gmail.com>
2024-04-06 20:17:42 -0500

    added

--- src/app/navbarhome/page.tsx
+++ src/app/navbarhome/page.tsx
@@ -5,13 +5,12 @@ import { User } from "@nextui-org/react";
 import {
   ContainerOutlined,
   DesktopOutlined,
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
   PieChartOutlined,
 } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 import { Divider } from "@nextui-org/react";
+import { Button, Link } from "@nextui-org/react";
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -41,31 +40,35 @@ const items: MenuItem[] = [
 
 export default function navbar() {
   return (
-    <div className="bg-[#001529] w-64 h-screen">
-
-      <div>
-        <User className='m-8'
-          name="Jane Doe"
-          description="Product Designer"
-          avatarProps={{
-            src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
-          }}
-        />
-      </div>
-      <Divider className="mb-8" />
-      <div>
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-          items={items}
-        />
-      </div>
-
-      <div>
+    <div className="bg-[#001529] w-64 h-screen place-content-between" >
+      <div className=''>
+        <div>
+          <User className='m-5'
+            name="Jane Doe"
+            description="Product Designer"
+            avatarProps={{
+              src: "https://i.pravatar.cc/150?u=a04258114e29026702d"
+            }}
+          />
+        </div>
         <Divider className="mb-8" />
+        <div>
+          <Menu
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            mode="inline"
+            theme="dark"
+            items={items}
+            className='mb-5'
+          />
+        </div>
+        <Divider className=" mb-8" />
 
+        <div className="flex gap-5 justify-center items-center">
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </div>
       </div>
     </div >
   );
